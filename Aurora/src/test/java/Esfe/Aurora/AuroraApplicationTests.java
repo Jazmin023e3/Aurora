20aba3dd6c16ed7fbdf764ffacf5e9616eefea75
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-08-16 17:38:01 -0600

    inicio

---
+++ Aurora/src/test/java/Esfe/Aurora/AuroraApplicationTests.java
@@ -0,0 +1,13 @@
+package Esfe.Aurora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
